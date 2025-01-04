0dd532fb4333bae8da7433d92c43c17d99ca919d
Murali Prasanth <mprasanth18@gmail.com>
2025-01-04 13:34:43 +0530

    updated icon of the webstie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,32 @@ const montserrat = Montserrat({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Yagava Guru Foundation',
   description: 'Empowering Lives, Preserving Traditions, Serving Humanity',
+  keywords: [
+    'Yagava Guru Foundation',
+    'Siddha Vedic Education',
+    'Community Service',
+    'Spiritual Foundation',
+    'Madhuyagav',
+  ],
+  openGraph: {
+    title: 'Yagava Guru Foundation',
+    description: 'Empowering Lives, Preserving Traditions, Serving Humanity',
+    type: 'website',
+    siteName: 'Yagava Guru Foundation',
+  },
+  metadataBase: new URL('https://yaagavaguru.com'),
+  robots: {
+    index: true,
+    follow: true,
+  },
   icons: {
-    icon: '/images/logo.png',
+    icon: [
+      {
+        url: '/images/logo.png',
+        type: 'image/png',
+      },
+    ],
+    shortcut: '/images/logo.png',
     apple: '/images/logo.png',
   },
 };
